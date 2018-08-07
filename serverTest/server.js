edf41a52515a684eb7cca0c931044f70a2e4fd46
mohsen158 <rahnamaei_158@yahoo.com>
2018-08-07 16:26:37 +0430

    update servertest

--- serverTest/server.js
+++ serverTest/server.js
@@ -1,28 +1,28 @@
 var WebSocketServer = require('ws').Server,
-const axios = require('axios');
-  wss = new WebSocketServer({port: 18123})
+   axios = require('axios');
+wss = new WebSocketServer({ port: 18123 })
 wss.on('connection', function (ws) {
-    console.log('one socket has connected')
+  console.log('one socket has connected')
   ws.on('message', function (message) {
-
+    console.log('message recive :',message);
     axios.post('http://nginx/', {
-      message:message
+      message: message
     })
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-   // console.log('Published', packet.payload);
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    // console.log('Published', packet.payload);
   });
-  
-  
 
-    console.log('received:', message)
-  })
+
+
+  console.log('received:', message)
+})
 //   setInterval(
 //     () => ws.send(`${new Date()}`),
 //     1000
 //   )
-})
+// })
